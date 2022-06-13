0ee03f7f1362dba7ee742e897cfa39d455b4c7f7
beyanon <tokabe333@gmail.com>
2022-06-13 19:17:30 +0900

    Update : Lesson13 100%

--- 13_FibonacciNumbers_FibFrog.rb
+++ 13_FibonacciNumbers_FibFrog.rb
@@ -3,11 +3,13 @@
 
 def solution(a)
     # write your code in Ruby 2.2
+    # n + 1 以下のフィボナッチ数列を列挙
+    # n + 1 がフィボナッチ数列に含まれているならば回数1で到達できる
     n = a.length
     fibo = []
     fibo << 1 
     fibo << 2
-    while fibo[-1] + fibo[-2] <= n 
+    while fibo[-1] + fibo[-2] <= n + 1
       fibo << fibo[-1] + fibo[-2]
     end
   
@@ -15,19 +17,24 @@ def solution(a)
     leaf = [1]
     leaf << a 
     leaf << [1]
-    leaf.flatten!
+    leaf.flatten! # [1, a1, a2, ... , an-1, an, 1] の形(両岸を1にする)
     n = leaf.length
-    arr = Array.new(n, big)
+    arr = Array.new(n, big) # 距離iに到達できる最小回数を記録
     arr[0] = 0
     0.upto(n - 1) do |i|
       next if arr[i] == big
       fibo.each do |f|
-        dist = i + f 
+        dist = i + f
+        #p dist
         next if dist >= n || leaf[dist] == 0
         arr[dist] = arr[i] + 1 if arr[dist] > arr[i] + 1
       end
     end
     
-    return arr[-1]
+    # p n
+    # p fibo 
+    # p leaf
+    # p arr
+    return arr[-1] != big ? arr[-1] : -1
   end
   
--- 99_test.rb
+++ 99_test.rb
@@ -1,15 +1,11 @@
-n = 100000
-fibo = {}
-fibo[1] = true
-fibo[2] = true 
-p1 = 1
-p2 = 2
-p3 = p1 + p2 
-while p3 < n 
-  fibo[p3] = true
-  p1 = p2 
-  p2 = p3 
-  p3 = p1 + p2 
+def fibo(n)
+  sq5 = Math.sqrt(5)
+  a = ((1 + sq5) / 2) ** n 
+  b = ((1 - sq5) / 2) ** n 
+  return ((a - b) / sq5).round
 end
-puts fibo.length
-p fibo
+
+
+20.times do |i|
+  puts "i:#{i} figo:#{fibo(i )}"
+end
